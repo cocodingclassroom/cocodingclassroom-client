c0185c089a9f2d5df8065ac68f6b007122d5d361
Marco Soldati <marco.soldati@fhnw.ch>
2023-06-29 13:22:42 +0200

    improve slider visuals.

--- src/components/modals/split-view-modal.js
+++ src/components/modals/split-view-modal.js
@@ -5,25 +5,19 @@ export const forceSplitView = () => {
   let value = UserService.get().localUser.leftSize;
   showModal(
     `
-      <div style="display: flex; flex-direction: column; justify-content: space-evenly; height: 150px">
+      <div style="display: flex; flex-direction: column; justify-content: space-evenly; height: 200px">
         <div>
-          Force Split View of Students
+          Set split screen
         </div>
-        <div style="display: flex; flex-direction: row; justify-content: space-between; width: 90%">
+        <div style="display: flex; flex-direction: row; justify-content: space-between; width: 90%; margin-top:30px;">
           <div>
-            <div>
-              Left
-            </div>
-            <div>
-              50%
+            <div id="left-split-label">
+              Teacher (50%)
             </div>
           </div>
           <div>
-            <div>
-              Right
-            </div>
-            <div>
-              50%
+            <div id="right-split-label">
+              Student (50%)
             </div>
           </div>
       </div>
@@ -36,6 +30,27 @@ export const forceSplitView = () => {
     },
     () => {
       let slider = document.getElementById("slider");
+      const leftLabel = document.getElementById("left-split-label");
+      const rightLabel = document.getElementById("right-split-label");
+      slider.addEventListener("input", (e) => {
+        let snap_value = e.target.value;
+        if (e.target.value < 10) {
+          snap_value = 0;
+        }
+        if (e.target.value > 45 && e.target.value < 55) {
+          snap_value= 50;
+        } 
+        if (e.target.value > 90) {
+          snap_value = 100;
+        }
+
+        slider.value = snap_value;
+
+        leftLabel.innerText = `Teacher (${snap_value}%)`;
+        rightLabel.innerText = `Student (${(100 - snap_value)}%)`;
+        slider.style.backgroundSize = snap_value + '% 100%'
+      });
+
       slider.addEventListener("change", (e) => {
         value = e.target.value;
       });
