b82792d5322bf0abaa3425a3f1642e66a97d892d
Christoph Vögele <christoph.voegele@fhnw.ch>
2023-10-27 14:32:20 +0200

    fix hydra code template indents

--- src/bindings/hydra/binding.js
+++ src/bindings/hydra/binding.js
@@ -4,17 +4,15 @@ export class binding extends BindingBase {
   bindingName = "Hydra";
 
   getCodeTemplate = () => {
-    return `
- osc(5, 0.9, 0.001)
- .kaleid([3,4,5,7,8,9,10].fast(0.1))
- .color(0.5, 0.3)
- .colorama(0.4)
- .rotate(0.009,()=>Math.sin(time)* -0.001 )
- .modulateRotate(o0,()=>Math.sin(time) * 0.003)
- .modulate(o0, 0.9)
- .scale(0.9)
- .out(o0)
-    `;
+    return `osc(5, 0.9, 0.001)
+.kaleid([3,4,5,7,8,9,10].fast(0.1))
+.color(0.5, 0.3)
+.colorama(0.4)
+.rotate(0.009,()=>Math.sin(time)* -0.001)
+.modulateRotate(o0,()=>Math.sin(time) * 0.003)
+.modulate(o0, 0.9)
+.scale(0.9)
+.out(o0)`;
   };
 
   getCustomCode = () => {
