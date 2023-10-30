ed3130160b243e85d27bc310c23b084330c858d2
Marco Soldati <marco.soldati@fhnw.ch>
2023-10-30 09:10:02 +0100

    https://trello.com/c/pT45xJxd/19-setup-roomcount type in 'Initial Room Count' instead of dropdown

--- src/components/setup.js
+++ src/components/setup.js
@@ -4,21 +4,21 @@ import { Router } from "@vaadin/router";
 import { safeRegister } from "../util/util";
 import { inputStyle } from "../util/shared-css";
 import { bindings } from "../bindings/bindings-config";
+import { number } from "lib0";
 
 export class Setup extends LitElement {
-  initialRoomNumbers = 5;
-  liveCodingOn = true;
-  secondsDelay = 0.5;
-  lineNumbersVisible = true;
-  roomLocksOn = false;
-  bindingIndex = 0;
-  password = "";
+    initialRoomNumbers = 5;
+    liveCodingOn = true;
+    secondsDelay = 0.5;
+    lineNumbersVisible = true;
+    roomLocksOn = false;
+    bindingIndex = 0;
+    password = "";
 
-  render() {
-    let roomNumbers = Array.from({ length: 16 }, (v, k) => k + 5);
-    let secDelays = Array.from({ length: 4 }, (v, k) => (k + 1) / 2);
-    let i = 0;
-    return html`
+    render() {
+        let secDelays = Array.from({ length: 4 }, (v, k) => (k + 1) / 2);
+        let i = 0;
+        return html`
       <div class="container-row">
         <div class="container-col">
           <h3 class="p5">COCODING CLASSROOM - SETUP</h3>
@@ -30,26 +30,23 @@ export class Setup extends LitElement {
               @change="${this.onChangeBinding}"
             >
               ${bindings.map(
-                (binding) => html` <option value="${i++}">
+            (binding) => html` <option value="${i++}">
                   ${new binding().bindingName}
                 </option>`
-              )}
+        )}
             </select>
             <label for="binding">Selected Binding</label>
           </div>
           <div class="p5">
-            <select
-              class="round submit"
-              name="rooms"
+            <input
+              class="round w55"
               @change="${this.onChangeRoomNumbers}"
-            >
-              ${roomNumbers.map(
-                (roomNumber) => html`
-                  <option value="${roomNumber}">${roomNumber}</option>
-                `
-              )}
-            </select>
-            <label for="rooms">initial Rooms</label>
+              @input="${this.onInputRoomNumbers}"
+              type="text"
+              value="5"
+              name="rooms"
+            />
+            <label for="rooms">initial Rooms (2-30)</label>
           </div>
           <hr />
           <div class="p5">
@@ -66,10 +63,10 @@ export class Setup extends LitElement {
                 class="round submit"
               >
                 ${secDelays.map(
-                  (secDelay) => html`
+            (secDelay) => html`
                     <option value="${secDelay}">${secDelay}</option>
                   `
-                )}
+        )}
               </select>
               sec delay</label
             >
@@ -121,54 +118,77 @@ export class Setup extends LitElement {
       </div>
       <slot></slot>
     `;
-  }
+    }
 
-  onChangeRoomNumbers = (e) => {
-    this.initialRoomNumbers = e.target.value;
-  };
+    onChangeRoomNumbers = (e) => {
+        // try parse value as number
+        var rooms = parseInt(e.target.value);
+        var value = rooms;
+        // if not a number
+        if (isNaN(rooms)) {
+            value = 5;
+        } else if (rooms < 2) {
+            value = 2;
+        } else if (rooms > 30) {
+            value = 30;
+        }
+        e.target.value = value;
+        this.initialRoomNumbers = value;
+    };
 
-  onChangeLiveDelay = (e) => {
-    this.liveCodingOn = e.target.checked;
-  };
+    onInputRoomNumbers = (e) => {
+        // try parse value as number
+        var rooms = parseInt(e.target.value);
+        // only allow integers
+        if (isNaN(rooms)) {
+            e.target.value = "";
+            return;
+        }
+        e.target.value = rooms;
+    }
 
-  onChangeSecondsDelay = (e) => {
-    this.secondsDelay = e.target.value;
-  };
+    onChangeLiveDelay = (e) => {
+        this.liveCodingOn = e.target.checked;
+    };
 
-  onChangeLineNumbers = (e) => {
-    this.lineNumbersVisible = e.target.value;
-  };
+    onChangeSecondsDelay = (e) => {
+        this.secondsDelay = e.target.value;
+    };
 
-  onChangeRoomLocks = (e) => {
-    this.roomLocksOn = e.target.checked;
-  };
+    onChangeLineNumbers = (e) => {
+        this.lineNumbersVisible = e.target.value;
+    };
 
-  onChangePassword = (e) => {
-    this.password = e.target.value;
-  };
+    onChangeRoomLocks = (e) => {
+        this.roomLocksOn = e.target.checked;
+    };
 
-  onChangeBinding = (e) => {
-    this.bindingIndex = e.target.value;
-  };
+    onChangePassword = (e) => {
+        this.password = e.target.value;
+    };
 
-  onSubmit = () => {
-    let newClassroomId = ClassroomService.get().createNewRoom(
-      () => {
-        console.log("created new room");
-        Router.go(`/${newClassroomId}`);
-      },
-      this.initialRoomNumbers,
-      this.liveCodingOn,
-      this.secondsDelay,
-      this.lineNumbersVisible,
-      this.bindingIndex,
-      this.roomLocksOn,
-      this.password
-    );
-  };
+    onChangeBinding = (e) => {
+        this.bindingIndex = e.target.value;
+    };
 
-  static styles = [
-    css`
+    onSubmit = () => {
+        let newClassroomId = ClassroomService.get().createNewRoom(
+            () => {
+                console.log("created new room");
+                Router.go(`/${newClassroomId}`);
+            },
+            this.initialRoomNumbers,
+            this.liveCodingOn,
+            this.secondsDelay,
+            this.lineNumbersVisible,
+            this.bindingIndex,
+            this.roomLocksOn,
+            this.password
+        );
+    };
+
+    static styles = [
+        css`
       .container-col {
         display: flex;
         flex-direction: column;
@@ -182,6 +202,10 @@ export class Setup extends LitElement {
 
       .p5 {
         padding: 5px;
+      }
+
+      .w55 {
+        width: 55px;
       }
 
       h3 {
@@ -214,8 +238,8 @@ export class Setup extends LitElement {
         font-family: inherit;
       }
     `,
-    inputStyle(),
-  ];
+        inputStyle(),
+    ];
 }
 
 safeRegister("cc-setup", Setup);
