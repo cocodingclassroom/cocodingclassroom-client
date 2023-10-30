a9fafe5274bcbca33aa39d67cb6cf8dcb7887355
Marco Soldati <marco.soldati@fhnw.ch>
2023-10-30 13:36:27 +0100

    https://trello.com/b/EkIkyXFl/cocoding?filter=+ disable button if room name is empty on room rename

--- src/components/modals/rename-room-modal.js
+++ src/components/modals/rename-room-modal.js
@@ -1,25 +1,33 @@
 import { RoomService } from "../../services/room-service";
 import { showModal } from "../../util/modal";
 
+
+
 export const renameRoom = (roomId) => {
-  let room = RoomService.get().getRoom(roomId);
-  showModal(
-    `
+    let room = RoomService.get().getRoom(roomId);
+    showModal(
+        `
       <div>
       Rename Room
       </div>
      <input id="roomname" class="cc-user-rename" name="roomname" type="text" value="${room.roomName.replace(
-       /["']/g,
-       ""
-     )}">
+            /["']/g,
+            ""
+        )}"
+     >
     `,
-    () => {
-      let nameInput = document.getElementById("roomname");
-      room.roomName = nameInput.value;
-    },
-    () => {
-      let nameInput = document.getElementById("roomname");
-      nameInput.select();
-    }
-  );
+        () => {
+            let nameInput = document.getElementById("roomname");
+            room.roomName = nameInput.value;
+        },
+        () => {
+            let nameInput = document.getElementById("roomname");
+            nameInput.addEventListener("input", (e) => {
+                let okButton = document.getElementById("modal-ok-button");
+                okButton.disabled = e.target.value.trim() === "";
+                console.log(okButton.disabled);
+            });
+            nameInput.select();
+        }
+    );
 };
